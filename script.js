7d5647457e58a4c4044b88a01389f91012aa4dc1
iasmylun <iasmynlungui@gmail.com>
2025-08-27 08:22:32 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Efeito simples de rolagem suave
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+  });
